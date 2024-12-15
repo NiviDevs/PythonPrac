ace6ee073f1dd61c592646902854af603e7f1b5e
Nividevs <nevedya.gwalani@gmail.com>
2024-12-15 17:57:32 +0530

    weird commit message ahahhaha

--- lmao.java
+++ lmao.java
@@ -2,5 +2,27 @@ public class lmao{
     public static void main(String args[]){
         System.out.println("Hello wOrld !");
 
+    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    int target = 5;
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = (start + end) / 2;
+    while(start <= end){
+        if(arr[mid] == target){
+            System.out.println("Found at index " + mid);
+            break;
+        }
+        else if(arr[mid] < target){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
     }
+    if(start > end){
+        System.out.println("Not found");
+
+    }
+}
 }
